8d8e34aba482609dd226e9e8f951a8ee816f51f0
Alexis Rangel <alexiscash97@rocketmail.com>
2025-04-04 17:13:51 -0500

    sssssssss

--- game.py
+++ game.py
@@ -1,23 +1,26 @@
 import os
+import time
 
 from snake import Snake
 
-UP = (0, 1)
-DOWN = (0, -1)
-RIGHT = (1, 0)
-LEFT = (-1, 0)
+UP = (-1, 0)
+DOWN = (1, 0)
+RIGHT = (0, 1)
+LEFT = (0, -1)
 
 
 class Game:
     def __init__(self, width: int, height: int):
         self.height: int = height
         self.width: int = width
+        self.ded = False
 
         self.board: list[list] = None
-        self._create_new_board()
+        self.create_new_board()
 
-        self.snake = Snake([(0, 0), (1, 0), (2, 0), (3, 0)], UP)
-        self._insert_snake()
+        self.snake = Snake(DOWN, [(3, 2), (3, 3), (3, 4), (3, 5)])
+        # self.snake = Snake(DOWN)
+        self.insert_snake()
 
     def render(self) -> None:
         os.system("clear")
@@ -28,22 +31,64 @@ class Game:
             print(f"|{"".join(row)}|")
         print(f"+{"-" * (self.width)}+")
 
-    def _create_new_board(self) -> None:
+    def loop(self) -> None:
+        for _ in range(20):
+            if self.ded is False:
+                self.render()
+                self.move_snake()
+                print(self.snake.head())
+                time.sleep(.5)
+            else:
+                print("ded")
+                return
+
+    def change_direction(self):
+        inp = input()
+        dir = None
+        match inp:
+            case "w":
+                dir = UP
+            case "s":
+                dir = DOWN
+            case "D":
+                dir = RIGHT
+            case "A":
+                dir = LEFT
+        return dir
+
+    def move_snake(self) -> None:
+        snake = self.snake
+        direction = snake.direction
+        head = snake.head()
+        new_position = (head[0] + direction[0], head[1] + direction[1])
+        snake.take_step(new_position)
+        if snake.head()[0] > self.height or snake.head()[0] < 0:
+            self.ded = True
+            return
+        if snake.head()[1] > self.width or snake.head()[1] < 0:
+            self.ded = True
+            return
+        self.create_new_board()
+        self.insert_snake()
+
+    def create_new_board(self) -> None:
         self.board = [[" " for _ in range(self.width)]
                       for _ in range(self.height)]
 
-    def _insert_snake(self) -> None:
+    def insert_snake(self) -> None:
         board = self.board
         body = self.snake.body
+        head = self.snake.head()
+        try:
+            board[head[0]][head[1]] = "X"
+            for s in body[:-1]:
+                body_x = s[0]
+                body_y = s[1]
+                board[body_x][body_y] = "0"
+        except IndexError:
+            self.ded = True
 
-        head_x = body[0][0]
-        head_y = body[0][0]
-        board[head_x][head_y] = "X"
-        for s in range(1, len(self.snake.body)):
-            body_x = body[s][0]
-            body_y = body[s][1]
-            board[body_x][body_y] = "O"
 
-
-game = Game(5, 7)
-game.render()
+if __name__ == "__main__":
+    game = Game(15, 10)
+    game.loop()
--- snake.py
+++ snake.py
@@ -1,18 +1,20 @@
 class Snake:
-    def __init__(self, init_body, init_direction, length: int = 3):
-        self.length: int = length
-        self.body: list[tuple] = init_body
-        self.direction: tuple = init_direction
+    def __init__(self, init_direction: tuple, init_body: list[tuple] = [], length: int = 3):
+        self.length = length
+        if init_body:
+            self.body = init_body
+        else:
+            self.body = [(0, 0), (1, 0), (2, 0), (3, 0)]
+        self.direction = init_direction
 
     def take_step(self, position):
-        self.body.insert(0, position)
-        self.body.pop()
+        self.body = self.body[1:] + [position]
 
     def set_direction(self, direction):
         self.direction = direction
 
     def head(self):
-        return self.body[0]
+        return self.body[-1]
 
 
 snake = Snake([(1, 1), (1, 2), (1, 3)], (2, 2))
